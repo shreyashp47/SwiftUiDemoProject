061f7bac8aa0d1ec11dec161896c6bab56f8c251
Shreyash <shreyashp47@gmail.com>
2024-01-02 07:17:10 +0530

    Initial Commit

---
+++ SwiftUiDemoProject/SwiftUiDemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiDemoProjectApp.swift
+//  SwiftUiDemoProject
+//
+//  Created by Shreyash Pattewar on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiDemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
